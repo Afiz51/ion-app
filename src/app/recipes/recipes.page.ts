1d0e99e90cad20867c89cece3f6972097564538b
gboye <afiz.lawal51@gmail.com>
2022-12-09 00:41:36 +0100

    recent updates

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'spaghetti',
+      imageUrl: 'https://veganwithgusto.com/wp-content/uploads/2021/05/speedy-spaghetti-arrabbiata-1st-image.jpg',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad'],
+    },
+    {
+      id: 'r2',
+      title: 'Schnitzel',
+      imageUrl: 'https://therecipecritic.com/wp-content/uploads/2020/10/pork-schnitzel-recipe-3.jpg',
+      ingredients: ['spaghetti', 'carrots', 'sauce'],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
